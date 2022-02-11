f06c52e61dbd7ff68cee86a335c2a0b8df9b6c3a
Gohar <gohar.mamunts@gmail.com>
2022-02-11 04:11:54 +0400

    adds backend function

--- sign_up/sign_up.js
+++ sign_up/sign_up.js
@@ -1,5 +1,6 @@
 window.onload = () => {
-  addInputHandlers();
+  // addInputHandlers();
+  backend();
 };
 
 const formElem = document.querySelector("form");
@@ -18,43 +19,103 @@ formElem.addEventListener("submit", (event) => {
   event.preventDefault();
 });
 
-const usersState = [];
-const userState = {
-  name: null,
-  surname: null,
-  email: null,
-  password: null,
-};
+// const usersState = [];
+// const userData = {
+//   name: null,
+//   surname: null,
+//   email: null,
+//   password: null,
+// };
 
-const inputs = document.querySelectorAll("input");
-const confirmPassword = document.getElementById("confirm-password");
-const signUpBtn = document.getElementById("sign-up-btn");
+function backend() {
+  const usersDataBase = [];
+  const userData = {
+    name: null,
+    surname: null,
+    email: null,
+    password: null,
+    // status
+  };
+  return {
+    signedUp() {
+      const inputs = document.querySelectorAll("input");
+      // const confirmPassword = document.getElementById("confirm-password");
+      const signUpBtn = document.getElementById("sign-up-btn");
 
-signUpBtn.addEventListener("click", () => {
-  if (userState.password !== userState.confirmPwd) {
-    dialogWarning.innerHTML = "Please recheck your password!";
-    dialog.classList.add("active");
-    overlay.classList.add("active");
-  }
-});
+      signUpBtn.addEventListener("click", () => {
+        if (userData.password !== userData.confirmPwd) {
+          dialogWarning.innerHTML = "Please recheck your password!";
+          dialog.classList.add("active");
+          overlay.classList.add("active");
+        }
+      });
 
-function makeInputChangeHandler(userState) {
-  function inputChangeHandler(event) {
-    const filledUpName = event.target.value;
-    for (let category in userState) {
-      if (category === event.target.name) {
-        userState[category] = filledUpName;
+      function makeInputChangeHandler(category) {
+        function inputChangeHandler(event) {
+          const filledUpName = event.target.value;
+          // for (let category in userData) {
+          if (category === event.target.name) {
+            userData[category] = filledUpName;
+          }
+          // }
+        }
+        usersDataBase.push(userData);
+        // console.log("usersDataBase", usersDataBase);
+        return inputChangeHandler;
       }
-    }
-  }
-  usersState.push(userState);
-  return inputChangeHandler;
+
+      function addInputHandlers() {
+        inputs.forEach((input) => {
+          const inputChangeHandler = makeInputChangeHandler(input.name);
+          input.addEventListener("change", inputChangeHandler);
+        });
+      }
+      console.log("usersDataBase", usersDataBase);
+      addInputHandlers();
+    },
+    signedIn() {},
+    signedOut() {},
+  };
 }
 
-const inputChangeHandler = makeInputChangeHandler(userState);
+const { signedUp } = backend();
+console.log(signedUp);
 
-function addInputHandlers() {
-  inputs.forEach((input) => {
-    input.addEventListener("change", inputChangeHandler);
-  });
-}
+// const inputs = document.querySelectorAll("input");
+// const confirmPassword = document.getElementById("confirm-password");
+// const signUpBtn = document.getElementById("sign-up-btn");
+
+// signUpBtn.addEventListener("click", () => {
+//   if (userState.password !== userState.confirmPwd) {
+//     dialogWarning.innerHTML = "Please recheck your password!";
+//     dialog.classList.add("active");
+//     overlay.classList.add("active");
+//   }
+// });
+
+// function makeInputChangeHandler(category) {
+//   function inputChangeHandler(event) {
+//     const filledUpName = event.target.value;
+//     for (let category in userState) {
+//       if (category === event.target.name) {
+//         userState[category] = filledUpName;
+//       }
+//     }
+//   }
+//   usersState.push(userState);
+//   return inputChangeHandler;
+// }
+
+// function addInputHandlers() {
+//   inputs.forEach((input) => {
+//     const inputChangeHandler = makeInputChangeHandler(input.name);
+//     input.addEventListener("change", inputChangeHandler);
+//   });
+// }
+
+// usersDataBase in backend function scope
+// get() createUser()
+// sign in() sign up() sign out()
+// {
+//   category: []
+// }
